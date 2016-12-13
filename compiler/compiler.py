806efd4349d13431abac6d0a8b1e036150038383
Simon Beal <simon.d.beal@gmail.com>
2016-12-13 17:54:58 +0000

    Now reads file from command line args

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,9 +1,11 @@
+import sys
+
 from tree_builder.tree_builder import build_tree
 from high_level_compiler.high_level_compiler import FileInterpreter as HighLevelFileInterpreter
 from low_level_compiler.low_level_compiler import FileInterpreter as LowLevelFileInterpreter
 
 if __name__ == "__main__":
-    high_level_file_interpreter = HighLevelFileInterpreter(build_tree("primes.txt"))
+    high_level_file_interpreter = HighLevelFileInterpreter(build_tree(sys.argv[1]))
     compiled = high_level_file_interpreter.compile()
     low_level_file_interpreter = LowLevelFileInterpreter(compiled,
                                                          high_level_file_interpreter.global_store)
