b913722c91a267153ea2c56dc5238732499ea153
mohdmalik02 <mohdmalik02@gmail.com>
2017-02-25 19:11:28 +0100

    forgot then

--- compiler/get_qftasm.sh
+++ compiler/get_qftasm.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 a=$(python3 compiler.py $1)
-if [[ "$OSTYPE" == "darwin"* ]];
+if [[ "$OSTYPE" == "darwin"* ]]; then
 	echo "$a" | grep "^\d" | pbcopy
 else
 	echo "$a" | grep "^\d" | xclip
